7750edf548b1fe351975eb8d5d1590ed9b0ab4f9
Sebastien0506 <124361814+Sebastien0506@users.noreply.github.com>
2025-05-23 19:22:26 +0200

    Ajout du code qui permet de recevoir la liste des employes

---
+++ src/app/admin-dashboard/admin-dashboard.component.ts
@@ -0,0 +1,28 @@
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { EmployesService } from '../employes/employes.service';
+import { AuthService } from '../auth-service.service';
+import { toSignal } from '@angular/core/rxjs-interop';
+
+@Component({
+  selector: 'app-admin-dashboard',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './admin-dashboard.component.html',
+  styleUrl: './admin-dashboard.component.css'
+})
+export class AdminDashboardComponent {
+  private authService = inject(AuthService);
+  role = toSignal(this.authService.role$);
+
+ 
+  constructor( private employesServices: EmployesService) {}
+
+
+  viewAllEmployer() {
+    this.employesServices.envoyerRequest().subscribe({
+      next: (res) => console.log("Employes reçus : ", res),
+      error: (err) => console.error("Erreur : ", err)
+    });
+  }
+}

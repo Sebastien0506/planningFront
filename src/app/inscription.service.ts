0e457c804ddbbc730d456372a6af7d266e7d0a51
Sebastien0506 <124361814+Sebastien0506@users.noreply.github.com>
2025-05-23 16:20:56 +0200

    Ajout du code pour l'inscription

--- src/app/inscription.service.ts
+++ src/app/inscription.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
+export interface InscriptionResponse {
+  message: string;
+}
 @Injectable({
   providedIn: 'root'
 })
+
 export class InscriptionService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  envoyerInscription(data: any) : Observable<InscriptionResponse> {
+    return this.http.post<InscriptionResponse>('http://localhost:8000/api/add_admin/', data, {
+      withCredentials: true
+    });
+  }
 }
--- src/app/inscription/inscription.component.ts
+++ src/app/inscription/inscription.component.ts
@@ -1,6 +1,9 @@
 import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
+import { InscriptionService } from '../inscription.service';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth-service.service';
 @Component({
   selector: 'app-inscription',
   standalone: true,
@@ -15,20 +18,23 @@ export class InscriptionComponent {
   email: string = "";
   password: string = "";
 
+  constructor(private inscriptionService: InscriptionService, private authService: AuthService, private router: Router){}
+
   validInscription(username: string, lastname: string, email: string, password: string): string {
+    //On crée une variable champs
     const champs = [
       { nom: 'nom', valeur: username },
       { nom: 'prénom', valeur: lastname },
       { nom: 'email', valeur: email },
       { nom: 'mot de passe', valeur: password },
     ];
-  
+  //On vérifie si tous les chanps sont de type string
     for (const champ of champs) {
       if (typeof champ.valeur !== 'string') {
         return `❌ Le champ '${champ.nom}' doit être une chaîne de caractères.`;
       }
     }
-  
+    //On vérifie si les caractères sont des caractères autorisé
     const isAlphaOnly = (str: string) => {
       for (let i = 0; i < str.length; i++) {
         const code = str.charCodeAt(i);
@@ -46,11 +52,11 @@ export class InscriptionComponent {
     if (!isAlphaOnly(lastname)) {
       return "❌ Le champ 'prénom' est invalide.";
     }
-  
+    
     if (!email.includes('@') || !email.includes('.')) {
       return '❌ Email invalide : il doit contenir @ et .';
     }
-  
+  //On vérifie que l'email contient que des caractères autorisé
     for (let i = 0; i < email.length; i++) {
       const code = email.charCodeAt(i);
       const isValidChar =
@@ -62,18 +68,21 @@ export class InscriptionComponent {
         return `❌ Email invalide : caractère non autorisé → ${email[i]}`;
       }
     }
-  
+    //On initialise les variales 'contientChiffre' et 'contientMajuscule' à false
     let contientChiffre = false;
     let contientMajuscule = false;
     let contientCaractereSpecial = false;
+
+    //On définit les caractères spéciaux autorisé pour le mot de passe
     const specialChars = [35, 36, 37, 64]; // #, $, %, @
-  
+
+  //On vérifie que le mot de passe contient bien une majuscule, un chiffre et un caractères spéciale
     for (let i = 0; i < password.length; i++) {
       const code = password.charCodeAt(i);
       if (code >= 65 && code <= 90) contientMajuscule = true;
       if (code >= 48 && code <= 57) contientChiffre = true;
       if (specialChars.includes(code)) contientCaractereSpecial = true;
-  
+      //On vérifie que le mot de passe contient que des caractères autorisé
       const isAllowedChar =
         (code >= 48 && code <= 57) || // chiffres
         (code >= 65 && code <= 90) || // majuscules
@@ -97,6 +106,39 @@ export class InscriptionComponent {
     console.log('Lastname : ', this.lastname);
     console.log('Email :', this.email);
     console.log('Password :', this.password);
+  }
+  //On crée la fonction qui permet l'envoie des données au back
+  envoyer() {
+    //On vérifie si les données sont valide
+    const estValide = this.validInscription(this.username, this.lastname, this.email, this.password);
+    if(!estValide) {
+      console.log("Données invalides, requête bloquée.");
+      return;
+    }
+    //On stock les données dans la variable data
+    const data = {
+      username: this.username,
+      last_name: this.lastname,
+      email: this.email,
+      password: this.password
+    };
+    //On envoie les données
+   this.inscriptionService.envoyerInscription(data).subscribe({
+    next: (res) => {
+      alert(res.message);
+
+      this.authService.fetchUserRole();
+      //On redirige l'utilisteur
+      setTimeout(() => this.router.navigate(['/']), 300);
+      
+
+    },
+    error: (err) => {
+      console.error('Erreur : ', err);
+      alert(err.message)
+    }
+    
+   });
   }
 }
 

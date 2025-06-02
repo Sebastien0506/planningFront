42debf714ca22ec6ccf636c0ac6d9b9e4bb33649
Sebastien0506 <124361814+Sebastien0506@users.noreply.github.com>
2025-06-02 13:36:06 +0200

    Modification du code pour afficher les lien vers les différente pages

--- src/app/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin-dashboard/admin-dashboard.component.ts
@@ -3,11 +3,11 @@ import { RouterLink } from '@angular/router';
 import { EmployesService } from '../employes/employes.service';
 import { AuthService } from '../auth-service.service';
 import { toSignal } from '@angular/core/rxjs-interop';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-admin-dashboard',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './admin-dashboard.component.html',
   styleUrl: './admin-dashboard.component.css'
 })
